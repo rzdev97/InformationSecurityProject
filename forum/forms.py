c65c941a35601f7ed2ad77b9db6e849490d8f10d
axelmezini <44057343+axelmezini@users.noreply.github.com>
2021-06-01 20:31:05 +0200

    Add files via upload

---
+++ forum/forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from forum.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()]) #Length(min=2, max=20)
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Sign Up")
+
+    @staticmethod
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is taken. Please choose a different one.")
+
+    #    excluded_chars = " *?!'^+%&/()=}][{$#@"
+    #    for char in username.data:
+    #        if char in excluded_chars:
+    #            raise ValidationError("Username can not contain these charachters: " + excluded_chars + ".")
+
+    @staticmethod
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is taken. Please choose a different one.")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+class QuestionForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Ask")
+
+
+class AnswerForm(FlaskForm):
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Answer")
